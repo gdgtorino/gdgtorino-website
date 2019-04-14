6e28c158ba8d38cb7140fc6b34ee620e08020c2d
Umberto Pepato <umbo.pepato@gmail.com>
2019-04-14 18:45:33 +0200

    Add helper method to eventbrite service

--- src/services/eventbrite.ts
+++ src/services/eventbrite.ts
@@ -1,14 +1,14 @@
-const baseUrl = 'https://www.eventbriteapi.com/v3/';
 import * as configuration from '../../config.json';
 
+const baseUrl = 'https://www.eventbriteapi.com/v3/';
 const {organizationId, token} = configuration.eventbrite;
+const fetchJson = (input: RequestInfo, init?: RequestInit) => fetch(input, init)
+    .then(r => r.json());
 
 export const fetchUpcomingEvents = () => {
-    return fetch(`${baseUrl}events/search/?token=${token}&organizer.id=${organizationId}&sort_by=-date&expand=venue`)
-        .then(r => r.json());
+    return fetchJson(`${baseUrl}events/search/?token=${token}&organizer.id=${organizationId}&sort_by=-date&expand=venue`);
 };
 
 export const fetchPastEvents = () => {
-    return fetch(`${baseUrl}organizers/${organizationId}/events/?token=${token}&order_by=start_desc&status=ended`)
-        .then(r => r.json());
+    return fetchJson(`${baseUrl}organizers/${organizationId}/events/?token=${token}&order_by=start_desc&status=ended`);
 };
