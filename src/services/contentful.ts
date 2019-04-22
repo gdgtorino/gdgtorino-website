b682a471a47712aed1788529b0f252e7847074a8
Umberto Pepato <umbo.pepato@gmail.com>
2019-04-22 20:31:21 +0200

    Move ordering at query time

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -1,30 +1,49 @@
-import * as contentful from 'contentful';
+import {ContentfulClientApi, createClient, Entry, EntryCollection} from 'contentful';
+import {
+    IGdgFields,
+    IOrganizerFields,
+    IPageFields,
+    IPartnerFields, ITeamFields,
+} from '../content-types/generated';
 import * as configuration from '../../config.json';
 
+const contentfulClient: ContentfulClientApi = createClient(configuration.contentful);
 
-const contentfulClient = contentful.createClient(configuration.contentful);
-
-export const getRoutingData = (): Promise<any> => {
-    return contentfulClient.getEntries({
+export const getRoutingData = (): Promise<EntryCollection<IPageFields>> => {
+    return contentfulClient.getEntries<IPageFields>({
         content_type: 'page',
         select: 'fields.slug,fields.component,fields.mainNavigationItem,fields.name,fields.order',
+        order: 'fields.order',
     });
 };
 
-export const getGdg = (): Promise<any> => {
-    return contentfulClient.getEntry('11pVnPX4AKqcCIAyyYwqI6');
+export const getGdg = (): Promise<Entry<IGdgFields>> => {
+    return contentfulClient.getEntry<IGdgFields>('11pVnPX4AKqcCIAyyYwqI6');
 };
 
-export const getPartners = (): Promise<any> => {
-    return contentfulClient.getEntries({
+export const getPartners = (): Promise<EntryCollection<IPartnerFields>> => {
+    return contentfulClient.getEntries<IPartnerFields>({
         'content_type': 'partner',
         select: 'fields.logo',
     });
 };
 
-export const getAbout = (): Promise<any> => {
-    return contentfulClient.getEntries({
+export const getAbout = (): Promise<EntryCollection<IPageFields>> => {
+    return contentfulClient.getEntries<IPageFields>({
         content_type: 'page',
         'fields.slug': 'about',
+    });
+};
+
+export const getOrganizers = (): Promise<Entry<IOrganizerFields>[]> => {
+    return contentfulClient.getEntries<IOrganizerFields>({
+        content_type: 'organizer',
+    }).then(data => data.items);
+};
+
+export const getTeams = (): Promise<EntryCollection<ITeamFields>> => {
+    return contentfulClient.getEntries<ITeamFields>({
+        content_type: 'team',
+        order: 'fields.order',
     });
 };
