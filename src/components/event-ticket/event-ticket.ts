5c786d468ca3855b471c8d4daeda7e02a9009668
Umberto Pepato <umbo.pepato@gmail.com>
2019-04-26 22:35:41 +0200

    Add narrow ticket version

--- src/components/event-ticket/event-ticket.ts
+++ src/components/event-ticket/event-ticket.ts
@@ -30,9 +30,12 @@ class EventTicket extends LitElement {
         return html`
             <svg width="0" height="0">
                 <defs>
-                    <clipPath id="ticketClipPath" clipPathUnits="objectBoundingBox" transform="scale(0.001, 0.00333)">
+                    <clipPath id="ticketClipPathOriz" clipPathUnits="objectBoundingBox" transform="scale(0.001, 0.00333)">
                         <path class="st0" d="M619.5,299.5c-0.3-10.5-8.9-19-19.5-19s-19.2,8.5-19.5,19H22.5c-12.1,0-22-9.9-22-22V22.5c0-12.1,9.9-22,22-22
 		                                     h558.1c0.3,10.5,8.9,19,19.5,19s19.2-8.5,19.5-19h358c12.1,0,22,9.9,22,22v255.1c0,12.1-9.9,22-22,22H619.5z"/>
+                    </clipPath>
+                    <clipPath id="ticketClipPathVert" clipPathUnits="objectBoundingBox" transform="scale(0.001, 0.0011628)">
+                        <path class="cls-1" d="M1000,466.39v-430A36.37,36.37,0,0,0,963.63,0H36.37A36.37,36.37,0,0,0,0,36.37V467.06a31.76,31.76,0,0,1,31.77,31.78A31.76,31.76,0,0,1,0,530.61v293A36.37,36.37,0,0,0,36.37,860H963.63A36.37,36.37,0,0,0,1000,823.63V529.94a31.78,31.78,0,0,1,0-63.55Z"/>
                     </clipPath>
                 </defs>
             </svg>
@@ -45,19 +48,21 @@ class EventTicket extends LitElement {
                   </div>
                 </div>
                 
-                <div class="info">
-                  <h3>${this.event.name.text}</h3>
-                  <div class="caption">
-                    <div>${dateString}</div>
-                    <div>Ore ${timeString}</div>
+                <div class="info-container">
+                  <div class="info">
+                    <h3>${this.event.name.text}</h3>
+                    <div class="caption">
+                      <div>${dateString}</div>
+                      <div>Ore ${timeString}</div>
+                    </div>
+                    ${this.event.venue ? html`
+                        <div class="venue caption">
+                          <div>${this.event.venue.name}</div>
+                          <div>${this.event.venue.address.localized_address_display}</div>
+                        </div>
+                    ` : null}
+                    <div class="description">${this.event.description.text}</div>
                   </div>
-                  ${this.event.venue ? html`
-                      <div class="caption">
-                        <div>${this.event.venue.name}</div>
-                        <div>${this.event.venue.address.localized_address_display}</div>
-                      </div>
-                  ` : null}
-                  <div class="description">${this.event.description.text}</div>
                 </div>
               </div>
             </a>
